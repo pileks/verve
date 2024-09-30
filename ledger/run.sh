bd74709ac48f5aa80f444be0a0c2e25b5b1d1ac6
Pileks <pilex13@gmail.com>
2024-10-01 01:44:52 +0200

    automatically set program

--- ledger/run.sh
+++ ledger/run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # Start solana-test-validator in the background
-solana-test-validator --reset > /dev/null &
+solana-test-validator --reset --bpf-program 7UD7pBcVkw2yJQzdm9eNTfeSC8uo2WhM6MdUzBXK48zv /ledger/program/aa_poc.so > /dev/null &
 
 # Wait for the validator to be ready (optional but recommended)
+echo "Wait 5 seconds for Solana ledger to wake up"
+
 sleep 5  # Give the validator some time to start
 
 # Run solana logs in the foreground
