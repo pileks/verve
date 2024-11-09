4565750794db922e4de5336004ed20fc0d35cacd
Pileks <jure@amber-it.co>
2024-11-09 15:24:04 +0100

    SDK cleanup, prep for dev

--- sdk/src/utils.ts
+++
@@ -1,7 +0,0 @@
-import { Transaction, VersionedTransaction } from "@solana/web3.js";
-
-export function isLegacyTransactionInstance(
-  transaction: Transaction | VersionedTransaction
-): transaction is Transaction {
-  return (transaction as VersionedTransaction).version === undefined;
-}
