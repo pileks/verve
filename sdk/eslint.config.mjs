0cf29039fe4b79f3c776283c90a057585acf26f5
Dujo Perdic <dujo.perdic1@gmail.com>
2024-11-12 18:54:34 +0100

    linting and formatting

---
+++ sdk/eslint.config.mjs
@@ -0,0 +1,15 @@
+import globals from "globals";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { ignores: ["node_modules/", "lib/", "*.json", "eslint.config.mjs"] },
+  { languageOptions: { globals: globals.browser } },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  { rules: { "linebreak-style": ["error", "unix"] } },
+];
