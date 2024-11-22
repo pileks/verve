a4c2a7a5aca119fdd4d5f53c73ac3a733a8716bb
Dujo Perdic <dujo.perdic1@gmail.com>
2024-11-22 12:26:53 +0100

    updated types and constants

--- sdk/src/utils/constants.ts
+++ sdk/src/utils/constants.ts
@@ -1,4 +1,10 @@
-import { PublicKey } from "@solana/web3.js";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
+import type { LightAccounts } from "./types";
+import {
+  defaultStaticAccountsStruct,
+  defaultTestStateTreeAccounts,
+  LightSystemProgram,
+} from "@lightprotocol/stateless.js";
 
 export const PROGRAM_ID: PublicKey = new PublicKey(
   "Y3Fdm2T4ipYdaFBKxQb8M4QE8EgpxWAMa7c3q72vQhn",
@@ -9,6 +15,24 @@ export enum AaError {
   WalletMismatch = "Wallet mismatch",
   InvalidGuardianSignature = "Invalid guardian signature",
 }
+
+export const LIGHT_STATE_TREE_ACCOUNTS = defaultTestStateTreeAccounts();
+
+export const LIGHT_ACCOUNTS: LightAccounts = {
+  accountCompressionAuthority:
+    defaultStaticAccountsStruct().accountCompressionAuthority,
+  accountCompressionProgram:
+    defaultStaticAccountsStruct().accountCompressionProgram,
+  cpiSigner: PublicKey.findProgramAddressSync(
+    [Buffer.from("cpi_authority")],
+    PROGRAM_ID,
+  )[0],
+  lightSystemProgram: LightSystemProgram.programId,
+  noopProgram: defaultStaticAccountsStruct().noopProgram,
+  registeredProgramPda: defaultStaticAccountsStruct().registeredProgramPda,
+  selfProgram: PROGRAM_ID,
+  systemProgram: SystemProgram.programId,
+};
 
 export const PDA_WALLET_SEED: Uint8Array = Buffer.from("w");
 export const PDA_WALLET_GUARDIAN_SEED: Uint8Array = Buffer.from("wg");
--- sdk/src/utils/types.ts
+++ sdk/src/utils/types.ts
@@ -12,3 +12,14 @@ export interface VerveInstruction {
   signerAccounts: Array<boolean>;
   programAccountIndex: number;
 }
+
+export interface LightAccounts {
+  accountCompressionAuthority: PublicKey;
+  accountCompressionProgram: PublicKey;
+  cpiSigner: PublicKey;
+  lightSystemProgram: PublicKey;
+  noopProgram: PublicKey;
+  registeredProgramPda: PublicKey;
+  selfProgram: PublicKey;
+  systemProgram: PublicKey;
+}
