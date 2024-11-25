302ab2f1cb997e976a334fe65e14be21535c1a6c
Dujo Perdic <dujo.perdic1@gmail.com>
2024-11-25 11:59:01 +0100

    created common funcitons

--- sdk/src/utils/functions.ts
+++ sdk/src/utils/functions.ts
@@ -1,4 +1,11 @@
+import type { PublicKey } from "@solana/web3.js";
 import { keccak_256 } from "@noble/hashes/sha3";
+import {
+  LightSystemProgram,
+  type CompressedAccount,
+  type CompressedProofWithContext,
+  type NewAddressParams,
+} from "@lightprotocol/stateless.js";
 import { PROGRAM_ID } from "./constants";
 
 export function deriveSeed(seeds: Uint8Array[]): Uint8Array {
@@ -9,7 +16,7 @@ export function deriveSeed(seeds: Uint8Array[]): Uint8Array {
   return hash;
 }
 
-function hashvToBn254FieldSizeBe(bytes: Uint8Array[]): Uint8Array {
+export function hashvToBn254FieldSizeBe(bytes: Uint8Array[]): Uint8Array {
   const hasher = keccak_256.create();
 
   for (const input of bytes) {
@@ -21,4 +28,48 @@ function hashvToBn254FieldSizeBe(bytes: Uint8Array[]): Uint8Array {
   hash[0] = 0;
 
   return hash;
+}
+
+export function createNewAddressOutputState(
+  address: PublicKey,
+): CompressedAccount[] {
+  return LightSystemProgram.createNewAddressOutputState(
+    Array.from(address.toBytes()),
+    PROGRAM_ID,
+  );
+}
+
+export function getNewAddressParams(
+  addressSeed: Uint8Array,
+  proof: CompressedProofWithContext,
+): NewAddressParams {
+  const addressMerkleTreeRootIndex =
+    proof.rootIndices[proof.rootIndices.length - 1];
+
+  if (!addressMerkleTreeRootIndex) {
+    throw "No addressMerkleTreeRootIndex";
+  }
+
+  const addressMerkleTreePubkey =
+    proof.merkleTrees[proof.merkleTrees.length - 1];
+
+  if (!addressMerkleTreePubkey) {
+    throw "No addressMerkleTreePubkey";
+  }
+
+  const addressQueuePubkey =
+    proof.nullifierQueues[proof.nullifierQueues.length - 1];
+
+  if (!addressQueuePubkey) {
+    throw "No addressQueuePubkey";
+  }
+
+  const addressParams: NewAddressParams = {
+    seed: addressSeed,
+    addressMerkleTreeRootIndex,
+    addressMerkleTreePubkey,
+    addressQueuePubkey,
+  };
+
+  return addressParams;
 }
