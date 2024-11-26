18da3357763448b679b3f3326efd2ae49293c718
Dujo Perdic <dujo@alakazam.io>
2024-11-26 11:54:38 +0100

    exec multiple instructions wip

--- compressed-program/programs/compressed-aa-poc/src/lib.rs
+++ compressed-program/programs/compressed-aa-poc/src/lib.rs
@@ -198,6 +198,78 @@ pub mod compressed_aa_poc {
         Ok(())
     }
 
+    pub fn exec_multiple_instructions<'info>(
+        ctx: LightContext<'_, '_, '_, 'info, ExecMultipleInstructions<'info>>,
+        instruction_data: Vec<Vec<u8>>,
+    ) -> Result<()> {
+        require!(
+            ctx.accounts
+                .guardian
+                .key()
+                .eq(&ctx.light_accounts.wallet_guardian.guardian.key()),
+            AaError::GuardianMismatch
+        );
+
+        require!(
+            ctx.accounts
+                .wallet
+                .key()
+                .eq(&ctx.light_accounts.wallet_guardian.wallet.key()),
+            AaError::WalletMismatch
+        );
+
+        msg!(
+            "Executing tx for AA wallet {}, approved by: {}",
+            ctx.accounts.wallet.key(),
+            ctx.accounts.guardian.key()
+        );
+
+        for (_i, data) in instruction_data.iter().enumerate() {
+            let verve_instruction: VerveInstruction = VerveInstruction::try_from_slice(&data)?;
+
+            let mut account_metas: Vec<AccountMeta> = vec![];
+
+            for (j, account_index) in verve_instruction.account_indices.iter().enumerate() {
+                let account_key: Pubkey = ctx.remaining_accounts[*account_index as usize].key();
+                let is_writable: bool = verve_instruction.writable_accounts[j];
+                let is_signer: bool = verve_instruction.signer_accounts[j];
+
+                let account_meta: AccountMeta = if is_writable {
+                    AccountMeta::new(account_key, is_signer)
+                } else {
+                    AccountMeta::new_readonly(account_key, is_signer)
+                };
+
+                account_metas.push(account_meta);
+            }
+
+            let program_account_index: &u8 = &verve_instruction.program_account_index;
+
+            let instruction: Instruction = Instruction {
+                accounts: account_metas,
+                data: verve_instruction.data,
+                program_id: ctx.remaining_accounts[*program_account_index as usize].key(),
+            };
+
+            let seed_guardian_key: Pubkey = ctx.accounts.seed_guardian.key();
+
+            let seeds: [&[u8]; 3] = [
+                &PDA_WALLET_SEED[..],
+                seed_guardian_key.as_ref(),
+                &[ctx.bumps.wallet][..],
+            ];
+
+            let signer_seeds: &[&[&[u8]]; 1] = &[&seeds[..]];
+
+            let cpi_accounts: Vec<AccountInfo<'info>> =
+                ctx.remaining_accounts[*program_account_index as usize + 1..].to_vec();
+
+            invoke_signed(&instruction, &cpi_accounts, signer_seeds)?;
+        }
+
+        Ok(())
+    }
+
     pub fn generate_idl_types_noop(_ctx: Context<GenerateIdls>, _types: Types) -> Result<()> {
         Ok(())
     }
@@ -320,6 +392,41 @@ pub struct ExecInstruction<'info> {
 
 #[light_accounts]
 pub struct ExecInstructionAlt<'info> {
+    /// CHECK: No need to have this account initialized lol
+    #[account(
+        seeds=[PDA_WALLET_SEED, seed_guardian.key().as_ref()],
+        bump
+    )]
+    pub wallet: AccountInfo<'info>,
+
+    #[light_account(
+        mut,
+        seeds=[PDA_WALLET_GUARDIAN_SEED, wallet.key().as_ref(), guardian.key().as_ref()],
+    )]
+    pub wallet_guardian: LightAccount<WalletGuardian>,
+
+    /// CHECK: we use this to determine the account we're invoking for.
+    /// The actual access check happens using wallet_guardian.
+    #[account()]
+    pub seed_guardian: AccountInfo<'info>,
+
+    #[account()]
+    pub guardian: Signer<'info>,
+
+    #[account(mut)]
+    #[fee_payer]
+    pub payer: Signer<'info>,
+
+    #[self_program]
+    pub self_program: Program<'info, crate::program::CompressedAaPoc>,
+
+    /// CHECK: Checked in light-system-program.
+    #[authority]
+    pub cpi_signer: AccountInfo<'info>,
+}
+
+#[light_accounts]
+pub struct ExecMultipleInstructions<'info> {
     /// CHECK: No need to have this account initialized lol
     #[account(
         seeds=[PDA_WALLET_SEED, seed_guardian.key().as_ref()],
